536bd4eaaccee995803b21365840a2dba7fa72d9
Dibyajyoti Dash <dj.dibyajyotidash@gmail.com>
2023-04-09 04:01:10 +0530

    Added .gitignore

--- src/main/kotlin/com/kotlin/LoopsAndRanges.kt
+++ src/main/kotlin/com/kotlin/LoopsAndRanges.kt
@@ -19,4 +19,25 @@ fun main() {
     // Skipping values in iteration
     for (i in reverseRange step 2) //
         println("reverse range with skip: $i")
+
+    exploreWhile()
+    exploreDoWhile()
+}
+
+fun exploreDoWhile() {
+    var i = 0
+    do {
+        println("Value of i is : $i")
+        i++
+    } while (i < 5)
+}
+
+fun exploreWhile() {
+    var x = 1
+    while (x < 5) {
+        println("Value of x is : $x")
+        x++
+    }
+
+
 }
